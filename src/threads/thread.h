022e3dc44ffdc44bf161b646cc56b497056c273d
Alan Han <alan.y.han@gmail.com>
2015-01-22 20:54:34 +0000

    Thread struct modifications to enable sleep

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -98,6 +98,10 @@ struct thread
     uint32_t *pagedir;                  /* Page directory. */
 #endif
 
+    /* Owned by devices/timer.c */
+    int sleep_time;                     /* Time until wake-up */
+    struct list_elem sleepy_elem;       /* Sleepy threads list pointer */
+
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
   };
