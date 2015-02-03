91b30ab6a9c531c68ca736975bd8d12efd26707c
Adam Gestwa <adam.gestwa13@imperial.ac.uk>
2015-02-03 17:30:21 +0000

    Fixed some syntax errors.

--- src/lib/kernel/fixed_point.c
+++ src/lib/kernel/fixed_point.c
@@ -1,8 +1,10 @@
 #include "fixed_point.h"
 
+#include <stdint.h>
+
 static const int TAIL_DIGITS = 14;
 
-static const int CONVERSION_FACTOR = 1 << TAIL_DIGITS;
+static int CONVERSION_FACTOR = 1 << TAIL_DIGITS;
 
 fixed_point
 to_fixed_point(int integer)
@@ -19,14 +21,8 @@ int to_integer_truncated(fixed_point fixed_point)
 
 int to_integer_rounded(fixed_point fixed_point)
 {
-	if(x >= 0)
-    {
-		  return (fixed_point.value + (CONVERSION_FACTOR/2));
-	  }
-  else
-    {
-      return (fixed_point.value - (CONVERSION_FACTOR/2));
-	  }
+  return (fixed_point.value >= 0) ? fixed_point.value + (CONVERSION_FACTOR/2)
+                                  : fixed_point.value - (CONVERSION_FACTOR/2);
 }
 
 fixed_point add_fixed_points(fixed_point x, fixed_point y)
@@ -47,20 +43,20 @@ fixed_point add_fixed_point_to_integer(fixed_point x, int n)
 {
   fixed_point new_fixed_point;
   new_fixed_point.value = x.value + (n*CONVERSION_FACTOR);
-  return new_fixed_point
+  return new_fixed_point;
 }
 
 fixed_point subtract_integer_from_fixed_point(fixed_point x, int n)
 {
   fixed_point new_fixed_point;
   new_fixed_point.value = x.value - (n*CONVERSION_FACTOR);
-  return new_fixed_point
+  return new_fixed_point;
 }
 
 fixed_point multiply_fixed_points(fixed_point x, fixed_point y)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = (((int_64_t) x.value) * y.value) / CONVERSION_FACTOR
+  new_fixed_point.value = (((int_64_t) x.value) * y.value) / CONVERSION_FACTOR;
   return new_fixed_point;
 }
 
@@ -74,13 +70,13 @@ fixed_point multiply_fixed_point_by_integer(fixed_point x, int n)
 fixed_point divide_fixed_points(fixed_point x, fixed_point y)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = ((int_64_t) x) * CONVERSION_FACTOR / y.value;
+  new_fixed_point.value = ((int_64_t) x.value) * CONVERSION_FACTOR / y.value;
   return new_fixed_point
 }
 
 fixed_point divide_fixed_point_by_integer(fixed_point x, int n)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = x / n;
+  new_fixed_point.value = x.value / n;
   return new_fixed_point;
 }
