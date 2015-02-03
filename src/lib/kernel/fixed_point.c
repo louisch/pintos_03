af9abaa40d48d0d38d3ca656ed28968f38b14db1
Louis Chan <louis058@gmail.com>
2015-02-03 22:36:58 +0000

    Adding extra whitespace to keep with code convention

--- src/lib/kernel/fixed_point.c
+++ src/lib/kernel/fixed_point.c
@@ -5,34 +5,34 @@
 static const int TAIL_DIGITS = 14;
 
 inline int
-conversion_factor(void)
+conversion_factor (void)
 {
   return 1 << TAIL_DIGITS;
 }
 
 fixed_point
-to_fixed_point(int integer)
+to_fixed_point (int integer)
 {
 	fixed_point returned_point;
-	returned_point.value = integer*conversion_factor();
+	returned_point.value = integer * conversion_factor ();
 	return returned_point;
 }
 
 int
-to_integer_truncated(fixed_point fixed_point)
+to_integer_truncated (fixed_point fixed_point)
 {
-	return fixed_point.value/conversion_factor();
+	return fixed_point.value / conversion_factor ();
 }
 
 int
-to_integer_rounded(fixed_point fixed_point)
+to_integer_rounded (fixed_point fixed_point)
 {
-  return (fixed_point.value >= 0) ? fixed_point.value + (conversion_factor()/2)
-                                  : fixed_point.value - (conversion_factor()/2);
+  return (fixed_point.value >= 0) ? fixed_point.value + conversion_factor () / 2
+                                  : fixed_point.value - conversion_factor () / 2;
 }
 
 fixed_point
-fixed_point_add(fixed_point x, fixed_point y)
+fixed_point_add (fixed_point x, fixed_point y)
 {
   fixed_point new_fixed_point;
   new_fixed_point.value = x.value + y.value;
@@ -40,7 +40,7 @@ fixed_point_add(fixed_point x, fixed_point y)
 }
 
 fixed_point
-fixed_point_subtract(fixed_point x, fixed_point y)
+fixed_point_subtract (fixed_point x, fixed_point y)
 {
   fixed_point new_fixed_point;
   new_fixed_point.value = x.value - y.value;
@@ -48,10 +48,10 @@ fixed_point_subtract(fixed_point x, fixed_point y)
 }
 
 fixed_point
-fixed_point_addi(fixed_point x, int n)
+fixed_point_addi (fixed_point x, int n)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = x.value + (n*conversion_factor());
+  new_fixed_point.value = x.value + n * conversion_factor ();
   return new_fixed_point;
 }
 
@@ -59,20 +59,20 @@ fixed_point
 fixed_point_subtracti(fixed_point x, int n)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = x.value - (n*conversion_factor());
+  new_fixed_point.value = x.value - n * conversion_factor ();
   return new_fixed_point;
 }
 
 fixed_point
-fixed_point_multiply(fixed_point x, fixed_point y)
+fixed_point_multiply (fixed_point x, fixed_point y)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = (((int64_t) x.value) * y.value) / conversion_factor();
+  new_fixed_point.value = ((int64_t) x.value) * y.value / conversion_factor ();
   return new_fixed_point;
 }
 
 fixed_point
-fixed_point_multiplyi(fixed_point x, int n)
+fixed_point_multiplyi (fixed_point x, int n)
 {
   fixed_point new_fixed_point;
   new_fixed_point.value = x.value * n;
@@ -80,15 +80,15 @@ fixed_point_multiplyi(fixed_point x, int n)
 }
 
 fixed_point
-fixed_point_divide(fixed_point x, fixed_point y)
+fixed_point_divide (fixed_point x, fixed_point y)
 {
   fixed_point new_fixed_point;
-  new_fixed_point.value = ((int64_t) x.value) * conversion_factor() / y.value;
+  new_fixed_point.value = ((int64_t) x.value) * conversion_factor () / y.value;
   return new_fixed_point;
 }
 
 fixed_point
-fixed_point_dividei(fixed_point x, int n)
+fixed_point_dividei (fixed_point x, int n)
 {
   fixed_point new_fixed_point;
   new_fixed_point.value = x.value / n;
--- src/lib/kernel/fixed_point.h
+++ src/lib/kernel/fixed_point.h
@@ -6,26 +6,26 @@ typedef struct
   int value;
 } fixed_point;
 
-fixed_point to_fixed_point(int integer);
+fixed_point to_fixed_point (int integer);
 
-int to_integer_truncated(fixed_point fixed_point);
+int to_integer_truncated (fixed_point fixed_point);
 
-int to_integer_rounded(fixed_point fixed_point);
+int to_integer_rounded (fixed_point fixed_point);
 
-fixed_point fixed_point_add(fixed_point x, fixed_point y);
+fixed_point fixed_point_add (fixed_point x, fixed_point y);
 
-fixed_point fixed_point_subtract(fixed_point x, fixed_point y);
+fixed_point fixed_point_subtract (fixed_point x, fixed_point y);
 
-fixed_point fixed_point_addi(fixed_point x, int n);
+fixed_point fixed_point_addi (fixed_point x, int n);
 
-fixed_point fixed_point_subtracti(fixed_point x, int n);
+fixed_point fixed_point_subtracti (fixed_point x, int n);
 
-fixed_point fixed_point_multiply(fixed_point x, fixed_point y);
+fixed_point fixed_point_multiply (fixed_point x, fixed_point y);
 
-fixed_point fixed_point_multiplyi(fixed_point x, int n);
+fixed_point fixed_point_multiplyi (fixed_point x, int n);
 
-fixed_point fixed_point_divide(fixed_point x, fixed_point y);
+fixed_point fixed_point_divide (fixed_point x, fixed_point y);
 
-fixed_point fixed_point_dividei(fixed_point x, int n);
+fixed_point fixed_point_dividei (fixed_point x, int n);
 
 #endif
