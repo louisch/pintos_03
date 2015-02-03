f743c1816337fc38f2c29d4830ae9a21447a9d1e
Anton Alenov <alenov@live.ru>
2015-02-03 12:55:33 +0000

    Priority update and remove functions added. \nCorrected priority_set to update base priority.

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,7 +89,7 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int priority;                       /* Priority. */
+    int priority;                       /* Base priority of thread. */
     int nice;                           /* Niceness. */
     fixed_point recent_cpu;             /* CPU time this received 'recently' */
     /* Ordered list of priorities the thread has. */
@@ -139,7 +139,9 @@ typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
-void thread_add_priority (struct thread*, int);
+void thread_give_priority (struct thread*, int);
+void thread_remove_priority (struct thread*, int);
+void thread_update_priority (struct thread*, int, int);
 void thread_set_priority (int);
 
 int thread_get_nice (void);
