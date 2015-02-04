59e8a40c95593fe30580fdf1b72a67b123c79f48
Louis Chan <louis.chan13@imperial.ac.uk>
2015-02-04 11:48:00 +0000

    Initialize nice and recent_cpu fields

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -494,6 +494,8 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
+  t->nice = 0;
+  t->recent_cpu = to_fixed_point (0);
   t->magic = THREAD_MAGIC;
 
   old_level = intr_disable ();
