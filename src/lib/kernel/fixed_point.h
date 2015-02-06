d600213aa798e6ebc079aee600e8564dbf9b38ed
Adam Gestwa <adam.gestwa13@imperial.ac.uk>
2015-02-06 13:07:28 +0000

    Changed fixed_point.h to use macros.
    Changed fixed_point from a struct to a typedef int.

--- src/lib/kernel/fixed_point.h
+++ src/lib/kernel/fixed_point.h
@@ -1,31 +1,42 @@
 #ifndef __LIB_KERNEL_FIXED_POINT
 #define __LIB_KERNEL_FIXED_POINT
 
-typedef struct
-{
-  int value;
-} fixed_point;
+typedef int fixed_point;
 
-inline fixed_point to_fixed_point (int integer);
+static const int TAIL_DIGITS = 14;
 
-inline int to_integer_truncated (fixed_point fixed_point);
+#define to_fixed_point(INTEGER) \
+    ((fixed_point)(INTEGER << TAIL_DIGITS))
 
-inline int to_integer_rounded (fixed_point fixed_point);
+#define to_integer_truncated(FIXED_POINT) \
+    FIXED_POINT.value >> TAIL_DIGITS)
 
-inline fixed_point fixed_point_add (fixed_point x, fixed_point y);
+#define to_integer_rounded(FIXED_POINT) \
+    ((FIXED_POINT.value >= 0) ? FIXED_POINT.value + conversion_factor () / 2  \
+                                  : FIXED_POINT.value - conversion_factor () / 2) >> \
+    TAIL_DIGITS)
 
-inline fixed_point fixed_point_subtract (fixed_point x, fixed_point y);
+#define to_integer_add(FIXED_POINT_A, FIXED_POINT_B) \
+    (FIXED_POINT_A.value + FIXED_POINT_B.value) 
 
-inline fixed_point fixed_point_addi (fixed_point x, int n);
+#define fixed_point_subtract(FIXED_POINT_A, FIXED_POINT_B) \
+    (FIXED_POINT_A.value - FIXED_POINT_B.value)
 
-inline fixed_point fixed_point_subtracti (fixed_point x, int n);
+#define fixed_point_addi(FIXED_POINT, INT) \
+    (FIXED_POINT.value + INT << TAIL_DIGITS)
 
-inline fixed_point fixed_point_multiply (fixed_point x, fixed_point y);
+#define fixed_point_subtracti(FIXED_POINT, INT) \
+    (FIXED_POINT.value - INT << TAIL_DIGITS)
 
-inline fixed_point fixed_point_multiplyi (fixed_point x, int n);
+#define fixed_point_multiply(FIXED_POINT_A, FIXED_POINT_B) \
+    (FIXED_POINT_A.value * FIXED_POINT_B.value >> TAIL_DIGITS)
 
-inline fixed_point fixed_point_divide (fixed_point x, fixed_point y);
+#define fixed_point_multiplyi(FIXED_POINT, INT) \
+    (FIXED_POINT.value * INT)
 
-inline fixed_point fixed_point_dividei (fixed_point x, int n);
+#define fixed_point_divide(FIXED_POINT_A, FIXED_POINT_B) \
+    (((int64_t) FIXED_POINT_A.value) << TAIL_DIGITS / FIXED_POINT_B)
 
+#define fixed_point_dividei(FIXED_POINT, INT) \
+    (FIXED_POINT.value / INT)
 #endif
