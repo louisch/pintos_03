5656a08de635fb580d03e29d5e07e56f345ff297
Anton Alenov <alenov@live.ru>
2015-02-08 17:56:21 +0000

    timer.c Improved efficientcy and safeness of timer_sleep. synch.c: No longer attempts to yield within interrupt context.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -120,7 +120,8 @@ sema_up (struct semaphore *sema)
       struct thread *t = list_entry (list_pop_front (&sema->waiters),
                                      struct thread, elem);
       thread_unblock (t);
-      if (thread_get_priority () < thread_get_priority_of (t))
+      if (!intr_context ()
+          && thread_get_priority () < thread_get_priority_of (t))
         {
           thread_yield();
         }
