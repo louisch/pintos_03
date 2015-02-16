42c18415d00585135442c14d6e39562dc7018716
Alan Han <alan.y.han@gmail.com>
2015-02-16 16:39:49 +0000

    Simple system call framework being implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,8 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -15,6 +17,67 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  /* No pointer checking here! */
+  uint32_t call_no = *((uint32_t*) (12345));
+
+  switch (call_no)
+  {
+    case (SYS_HALT):
+      break;
+    case (SYS_EXIT):
+      break;
+    case (SYS_EXEC):
+      break;
+    case (SYS_WAIT):
+      break;
+    case (SYS_CREATE):
+      break;
+    case (SYS_REMOVE):
+      break;
+    case (SYS_OPEN):
+      break;
+    case (SYS_FILESIZE):
+      break;
+    case (SYS_READ):
+      break;
+    case (SYS_WRITE):
+      break;
+    case (SYS_SEEK):
+      break;
+    case (SYS_TELL):
+      break;
+    case (SYS_CLOSE):
+      break;
+    default:
+      /* Unknown system call encountered! */
+      ASSERT (0);
+  }
+
+  // uint32_t arg0 = *(((uint32_t*) (f->esp)) + 1);
+  // uint32_t arg1 = *(((uint32_t*) (f->esp)) + 2);
+  // uint32_t arg2 = *(((uint32_t*) (f->esp)) + 3);
+
+  // printf("Call no: %d, %d, %d, %d\n", call_no, arg0, arg1, arg2);
+
+  // printf ("asdfasdf??\n");
   printf ("system call!\n");
   thread_exit ();
+
+
+}
+
+// static function that gets args
+
+static void *syscall_check_pointer (void *uaddr)
+{
+  if (is_user_vaddr (uaddr) && pagedir_get_page(thread_current()->pagedir, uaddr))
+    {
+      printf("yays\n");
+      return uaddr;
+    }
+  else
+    {
+      printf("asdfasdfasdfrrrgh\n");
+      ASSERT(0);
+    }
 }
