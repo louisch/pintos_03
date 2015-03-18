f55c691c1ca6f076480d6721418aca8faff91d5b
Adam Gestwa <adam.gestwa13@imperial.ac.uk>
2015-03-18 18:43:36 +0000

    Finished implementation of mmap and hash functions also included.

--- src/userprog/mapped_files.h
+++ src/userprog/mapped_files.h
@@ -6,4 +6,18 @@
 mapid_t syscall_mmap (int fd, void *addr);
 void syscall_munmap (mapid_t mapid);
 
+
+struct mapid
+  {
+    mapid_t mapidid;
+    struct file *file;
+    struct hash_elem elem;
+  }
+  
+/* Mapid hash related functions */
+static unsigned mapid_hash_func (const struct hash_elem *e void *aux UNUSED);
+static void mapid_hash_destroy (struct hash_elem *e, void *aux UNUSED);
+static bool mapid_less_func (const struct hash_elem *a,
+                          const struct hash_elem *b,
+                          void *aux UNUSED);
 #endif
