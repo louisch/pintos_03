415a865dd0932b5a6a9be8423712e5b16bfb753f
Adam Gestwa <adam.gestwa13@imperial.ac.uk>
2015-03-19 18:18:55 +0000

    Fixed some minor errors in mmap and added stub for munmap

--- src/userprog/mapped_files.c
+++ src/userprog/mapped_files.c
@@ -1,9 +1,11 @@
 #include <stdbool.h>
 
+#include "threads/malloc.h"
 #include "userprog/mapped_files.h"
 #include "userprog/process.h"
 #include "filesys/file.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 
 
@@ -11,13 +13,15 @@ mapid_t
 syscall_mmap (int fd, void *addr)
 {
 	struct thread *t = thread_current ();
-	struct file *file = process_fetch_file (fd);
-
   if (fd == STDIN || fd == STDOUT)
     return -1;
 
+	struct file *file = process_fetch_file (fd);
+  if (file == NULL)
+    return -1;
+
 	off_t size = file_length (file);
-	if (size == 0 || size == NULL)
+	if (size == 0)
 		return -1;
   if ((int) addr == 0)
     return -1;
@@ -27,34 +31,23 @@ syscall_mmap (int fd, void *addr)
 	int num_of_pages = (size % PGSIZE != 0) ? 
 					   size / PGSIZE + 1 : 
 					   size / PGSIZE;
-	struct supp_page_entry buffer [num_of_pages ];
-	buffer = supp_page_lookup_range (&t->supp_page_table, addr, buffer, num_of_pages );
-	int index = 0;
-  while (index != num_of_pages )
-		{
-			if (buffer[index] == NULL)
-				{
-					return -1;
-				}
-      index++;
-		}
 
   process_info *process = process_current ();
-  int id = process->mapid++;
+  int id = process->mapid_counter;
+  process->mapid_counter++;
   struct hash *mapped_files = &process->mapped_files;
   struct mapid *mapid = malloc (sizeof(struct mapid));
   if (mapid == NULL)
-    return ABNORMAL_EXIT_STATUS;
+    return -1;
 
-  bool writable = TRUE;
-  int index = 0
+  bool writable = true;
+  int index = 0;
   int offset = 0;
   size_t remaining_bytes = size;
   while (index != num_of_pages)
     {
-      offset = index * PGSIZE
-      struct supp_page entry *entry = 
-        supp_page_create_entry (&t->supp_page_table, addr + offset, writable);
+      offset = index * PGSIZE;
+      struct supp_page_entry *entry = supp_page_create_entry (&t->supp_page_table, addr + offset, writable);
       supp_page_set_file_data (entry, file, offset, 
         remaining_bytes - (num_of_pages - index + 1)*PGSIZE);
       index++;
@@ -65,24 +58,30 @@ syscall_mmap (int fd, void *addr)
 	return id;
 }
 
+void 
+syscall_munmap (mapid_t mapping){
 
-static unsigned
-mapid_hash_func (const struct hash_elem *e void *aux UNUSED)
-{
-  return (unsigned) hash_entry (e, struct map_id)->mapid
 }
 
-static unsigned
+
+unsigned
+mapid_hash_func (const struct hash_elem *e, void *aux UNUSED)
+{
+  return (unsigned) hash_entry (e, struct mapid, elem)->mapid;
+}
+
+bool
 mapid_less_func (const struct hash_elem *a,
               const struct hash_elem *b,
               void *aux UNUSED)
 {
-  return hash_entry (a, struct mapid, elem)->fd
-         < hash_entry (b, struct mapid, elem)->fd;
+  return hash_entry (a, struct mapid, elem)->mapid
+         < hash_entry (b, struct mapid, elem)->mapid;
 }
 //Not sure about this yet.
-static void
-mapid_hash_destroy (struct hash_elem *e void *aux UNUSED)
+void
+mapid_hash_destroy (struct hash_elem *e, void *aux UNUSED)
 {
   struct mapid *mapid = hash_entry (e, struct mapid, elem);
+  free (mapid);
 }
