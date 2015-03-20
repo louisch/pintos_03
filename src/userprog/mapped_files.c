92608267e64b3833b86dccb6ae040919a05d0097
Louis Chan <louis.chan13@imperial.ac.uk>
2015-03-20 14:45:16 +0000

    Improve code quality of mapped_files

--- src/userprog/mapped_files.c
+++ src/userprog/mapped_files.c
@@ -8,55 +8,53 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-
-
 mapid_t
 syscall_mmap (int fd, void *addr)
 {
-	struct thread *t = thread_current ();
   if (fd == STDIN || fd == STDOUT)
-    return -1;
+    {
+      return MAP_FAILED;
+    }
 
+	struct thread *t = thread_current ();
 	struct file *file = process_fetch_file (fd);
   if (file == NULL)
-    return -1;
+    {
+      return MAP_FAILED;
+    }
 
 	off_t size = file_length (file);
-	if (size == 0)
-		return -1;
-  if ((int) addr == 0)
-    return -1;
-	if ((int) addr % PGSIZE != 0)
-		return -1;
+	if (size == 0 || (int) addr == 0 || (int) addr % PGSIZE != 0)
+    {
+      return MAP_FAILED;
+    }
 
-	int num_of_pages = (size % PGSIZE != 0) ? 
-					   size / PGSIZE + 1 : 
+	int num_of_pages = (size % PGSIZE != 0) ?
+					   size / PGSIZE + 1 :
 					   size / PGSIZE;
-             
+
   int size_data = num_of_pages * PGSIZE;
 
   process_info *process = process_current ();
   int id = process->mapid_counter;
   process->mapid_counter++;
   struct hash *mapped_files = &process->mapped_files;
-  struct mapid *mapid = malloc (sizeof(struct mapid));
-  if (mapid == NULL)
-    return -1;
+  struct mapid *mapid = try_calloc (1, sizeof *mapid);
 
   bool writable = true;
-  // printf("Got here in mmap\n");
-  struct supp_page_segment *segment = 
+  struct supp_page_segment *segment =
     supp_page_create_segment (&t->supp_page_table, addr, writable, size_data);
   supp_page_set_file_data (segment, file, 0, size);
 
-	mapid->mapid = id;
+  mapid->mapid = id;
 	mapid->file = file;
 	hash_insert (mapped_files, &mapid->elem);
-	return id;
+  return id;
 }
 
-void 
-syscall_munmap (mapid_t mapping){
+void
+syscall_munmap (mapid_t mapping)
+{
   process_info *process = process_current ();
   struct mapid mapid;
   struct hash_elem *e;
