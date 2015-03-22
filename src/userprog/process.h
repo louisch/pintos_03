dc6853d5ca8de20a03793cf7588927fb1798a2d0
Louis Chan <louis.chan13@imperial.ac.uk>
2015-03-22 14:38:30 +0000

    Indent process.h structs

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -13,53 +13,53 @@
 
 /* Data for a process used for syscalls. */
 typedef struct process_info
-{
-  /* Tracks the process's children, running or terminated,
-     which have not been waited on. */
-  struct hash children;
-  /* Pointer to parent's persistent_info struct. */
-  struct persistent_info *persistent;
+  {
+    /* Tracks the process's children, running or terminated,
+         which have not been waited on. */
+    struct hash children;
+    /* Pointer to parent's persistent_info struct. */
+    struct persistent_info *persistent;
 
-  /* For placing process_info in hash table mapping pids to process_info. */
-  struct hash_elem process_elem;
+    /* For placing process_info in hash table mapping pids to process_info. */
+    struct hash_elem process_elem;
 
-  /* Provides unique files descriptors for process. */
-  unsigned fd_counter;
-  /* Hash used to remember files open by process by their fd. */
-  struct hash open_files;
+    /* Provides unique files descriptors for process. */
+    unsigned fd_counter;
+    /* Hash used to remember files open by process by their fd. */
+    struct hash open_files;
 
 #ifdef VM
-  /* Hash used to for mapping ids to files */
-  struct hash mapped_files;
-  /* Provides unique map ids for process */
-  unsigned mapid_counter;
+    /* Hash used to for mapping ids to files */
+    struct hash mapped_files;
+    /* Provides unique map ids for process */
+    unsigned mapid_counter;
 #endif
-
-} process_info;
+  }
+process_info;
 
 #include "threads/thread.h"
 
 /* Data about a process's child, carried by the parent process_info. */
 typedef struct persistent_info
-{
-  /* Child process's ID, used when checking if a child belongs to a parent. */
-  pid_t pid;
-  /* Exit status of thread. */
-  int exit_status;
-  /* Indicates whether the child process is still running. */
-  bool running;
-  /* Reference counter. */
-  int reference_counter;
-  /* Lock to synchronise reads/writes to the persistent_info's fields. */
-  struct lock persistent_info_lock;
-  /* Semaphore used by parents to wait on children. */
-  struct semaphore wait_sema;
-  /* Pointer to child process_info. */
-  process_info *process_info;
-  /* Hash elem to be placed into process_info's children hash. */
-  struct hash_elem persistent_elem;
-
-} persistent_info;
+  {
+    /* Child process's ID, used when checking if a child belongs to a parent. */
+    pid_t pid;
+    /* Exit status of thread. */
+    int exit_status;
+    /* Indicates whether the child process is still running. */
+    bool running;
+    /* Reference counter. */
+    int reference_counter;
+    /* Lock to synchronise reads/writes to the persistent_info's fields. */
+    struct lock persistent_info_lock;
+    /* Semaphore used by parents to wait on children. */
+    struct semaphore wait_sema;
+    /* Pointer to child process_info. */
+    process_info *process_info;
+    /* Hash elem to be placed into process_info's children hash. */
+    struct hash_elem persistent_elem;
+  }
+persistent_info;
 
 persistent_info *process_execute_aux (const char *file_name);
 
